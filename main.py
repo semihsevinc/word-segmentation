9079e65e975f65488fd5b9429ed5f7dbfb746929
semihsevinc <semihsevinc24@gmail.com>
2021-10-13 22:37:01 +0300

    revision

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from PIL import Image
 import cv2
-import page
-
-import words
-from utils import *
-import utils
 import numpy as np
 import os
+import page
+import words
+from utils import *
 
 
 folder_path = r'segmented'
@@ -26,20 +24,22 @@ rgb_planes = cv2.split(image)
 result_planes = []
 result_norm_planes = []
 for plane in rgb_planes:
-    dilated_img = cv2.dilate(plane, np.ones((5,5), np.uint8))
-    bg_img = cv2.medianBlur(dilated_img, 21)
-    diff_img = 255 - (cv2.absdiff(plane, bg_img) )
-    norm_img = cv2.normalize(diff_img,None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
+    dilated_img = cv2.dilate(plane, np.ones((3,3), np.uint8))
+    bg_img = cv2.medianBlur(dilated_img, 7)
+    diff_img = 255 - (cv2.absdiff(plane, bg_img, cv2.CV_32S))
+    norm_img = cv2.normalize(diff_img, diff_img, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC4)
     result_planes.append(diff_img)
     result_norm_planes.append(norm_img)
 result = cv2.merge(result_planes)
 shadows_out_image = cv2.merge(result_norm_planes)
 
     ##### shadow removed from image #####
-
+thresh = 230
+shadows_out_image = cv2.threshold(shadows_out_image, thresh, 255, cv2.THRESH_BINARY)[1]
 # shadows out image saved JUST IN CASE #
 cv2.imwrite('image/shadows_out_image.png', shadows_out_image)
 cv2.imwrite('image/diff_img.png', result)
+cv2.imwrite('image/image.png', image)
 
 
 # Crop image and get limiting lines of boxes
